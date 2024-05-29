84910d559fb23cbe9fbfc7007e19d2be592178c2
Bohdan11Dii <bodyabrovdiy@gmail.com>
2024-05-29 20:08:18 +0200

    Use ModelSerializer in serializers.py

--- task/serializers.py
+++ task/serializers.py
@@ -2,14 +2,8 @@ from rest_framework import serializers
 from task.models import Category
 
 
-class CategorySerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(required=True, max_length=100)
+class CategorySerializer(serializers.ModelSerializer):
     
-    def create(self, validated_data):
-        return Category.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.save()
-        return instance
+    class Meta:
+        model = Category
+        fields = ("id", "name")
